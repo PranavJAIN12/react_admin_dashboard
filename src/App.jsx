7a3fe045fb37092d77632e6337e55ce617ccf7c2
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2024-09-29 11:57:31 +0530

    first one

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import { Routes, Route } from 'react-router-dom'
+import Overview from './pages/Overview'
+import Product from './pages/Product'
+import './App.css'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+
+      <div className='fixed inset-0 z-0'>
+        <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80'/>
+        <div className='absolute inset-0 ' />
+      </div>
+
+      <Sidebar/>
+        <Routes>
+          <Route path='/' element={<Overview />}/>
+          <Route path='/product' element={<Product />}/>
+        </Routes>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
